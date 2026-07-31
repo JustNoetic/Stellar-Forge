2b948e96c3911954706cd5a876428bfc119ff5c6
Noetic <just.noetic@gmail.com>
2026-07-31 14:41:07 +0700

    Fixed error

--- engine/main.py
+++ engine/main.py
@@ -1,5 +1,8 @@
-import traceback
+import os
 import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+import traceback
 import faulthandler
 from app import App
 
